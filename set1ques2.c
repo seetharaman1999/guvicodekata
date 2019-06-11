a93522f3619eb17ecbc34647f9a8c8c4a48f377f
seetharaman1999 <46376285+seetharaman1999@users.noreply.github.com>
2019-06-11 09:26:04 +0530

    Update set1ques2.c

--- set1ques2.c
+++ set1ques2.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int num;
-scanf("%d",&num);
-if(num<=-1)
+int number;
+scanf("%d",&number);
+if(number<=-1)
   printf("Invalid");
+else if(number%2==0)
+  printf("Even");
 else if(num%2!=0)
   printf("Odd");
-else if(num%2==0)
-  
-  printf("Even");
 return 0;
 }
