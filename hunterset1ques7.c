769a73adec2315cfafeff2eb50931ace13afdf70
seetharaman1999 <46376285+seetharaman1999@users.noreply.github.com>
2019-06-11 11:52:39 +0530

    Create hunterset1ques7.c

---
+++ hunterset1ques7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,j;
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++)
+  scanf("%d",&arr[i]);
+  for(i=0;i<n;i++)
+  {
+      if(i%2==0)
+      {
+        if(arr[i]%2!=0)
+        printf("%d",arr[i]);
+      }
+      else
+      {
+        if(arr[i]%2==0)
+        printf("%d",arr[i]);
+      }
+   }
+}
