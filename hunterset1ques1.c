aa617d074bde41fd7a491d846795e16c14a2662b
seetharaman1999 <46376285+seetharaman1999@users.noreply.github.com>
2019-06-11 10:53:59 +0530

    Create hunterset1ques1.c

---
+++ hunterset1ques1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,arr1[1000]={0},flag=0;
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&arr[i]);
+    arr1[arr[i]]++;
+   }
+   for(i=0;i<1000;i++)
+   {
+      if(arr1[i]>1)
+      {
+        flag=1;
+        printf("%d ",i);
+       }
+   }
+   if(flag==0)
+   printf("unique");
+   return 0;
+  }
+        
+        
+   
